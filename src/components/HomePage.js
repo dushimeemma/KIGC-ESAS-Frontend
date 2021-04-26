a310749ba9acf8afd504ec1d0eaaf8ebca450220
dushimeemma <dushimeemma@gmail.com>
2021-04-26 19:16:39 +0200

    feat(viewSeat): view room
    - integrate view room endpoint
    - refoctor response
    
    [Delivers #177839132]

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import {
   Row,
   Col,
@@ -11,11 +11,11 @@ import {
   ListGroup,
   ListGroupItem,
   Spinner,
-} from "reactstrap";
-import { useDispatch, useSelector } from "react-redux";
+} from 'reactstrap';
+import { useDispatch, useSelector } from 'react-redux';
 
-import HomeImage from "./Image";
-import { viewSeat } from "../actions/seat";
+import HomeImage from './Image';
+import { viewSeat } from '../actions/seat';
 
 const HomePage = () => {
   const dispatch = useDispatch();
@@ -25,15 +25,15 @@ const HomePage = () => {
     (state) => state.errors.msg.error || state.errors.msg.msg
   );
   const [state, setState] = React.useState({
-    reg: "",
+    reg: '',
   });
   const [isSubmitting, setIsSubmitting] = React.useState(false);
-  const [backErr, setBackErr] = React.useState("");
+  const [backErr, setBackErr] = React.useState('');
   React.useEffect(() => {
     setBackErr(backErrors);
     setIsSubmitting(false);
     setTimeout(() => {
-      setBackErr("");
+      setBackErr('');
     }, 15000);
   }, [backErrors]);
 
@@ -52,7 +52,7 @@ const HomePage = () => {
       dispatch(viewSeat(state));
       setTimeout(() => {
         window.location.reload(false);
-      }, 16000);
+      }, 6000);
     }
   }, [isSubmitting]);
 
@@ -63,16 +63,16 @@ const HomePage = () => {
   if (Object.keys(seat).length > 0) {
     setTimeout(() => {
       window.location.reload(false);
-    }, 15000);
+    }, 5000);
   }
 
   const { reg } = state;
   return (
-    <Row className="main-height">
+    <Row className='main-height'>
       <HomeImage />
-      <Col className="">
-        <div className=" border p-2 aside back-color">
-          <h3 className="text-center mt-2 mb-2">
+      <Col className=''>
+        <div className=' border p-2 aside back-color'>
+          <h3 className='text-center mt-2 mb-2'>
             Enter Your Registration Number <br />
             To View Your Seat
           </h3>
@@ -81,33 +81,31 @@ const HomePage = () => {
             <FormGroup>
               <Label>Registration Number</Label>
               <Input
-                type="text"
-                name="reg"
+                type='text'
+                name='reg'
                 onChange={onChange}
                 value={reg}
-                placeholder="eg: D/BCS/17/09/6177"
+                placeholder='eg: D/BCS/17/09/6177'
               />
             </FormGroup>
-            <Button className="btn btn-secondary btn-block mb-2">
-              {isSubmitting ? <Spinner color="light" size="sm" /> : "View Seat"}
+            <Button className='btn btn-secondary btn-block mb-2'>
+              {isSubmitting ? <Spinner color='light' size='sm' /> : 'View Seat'}
             </Button>
           </Form>
           {backErr ? (
-            <Alert className="text-center" color="danger">
+            <Alert className='text-center' color='danger'>
               {backErr}
             </Alert>
           ) : null}
           {Object.keys(seat).length > 0 ? (
             <ListGroup>
-              <ListGroupItem color="success">Name: {seat.name}</ListGroupItem>
-              <ListGroupItem color="success">RegNo: {seat.regNo}</ListGroupItem>
-              <ListGroupItem color="success">Level: {seat.level}</ListGroupItem>
-              <ListGroupItem color="success">
-                Course: {seat.course.name}
+              <ListGroupItem color='success'>Name: {seat.name}</ListGroupItem>
+              <ListGroupItem color='success'>RegNo: {seat.regNo}</ListGroupItem>
+              <ListGroupItem color='success'>Level: {seat.level}</ListGroupItem>
+              <ListGroupItem color='success'>
+                Course: {seat.course}
               </ListGroupItem>
-              <ListGroupItem color="success">
-                Seat: {seat.seat.seatNumber}-{seat.seat.room}
-              </ListGroupItem>
+              <ListGroupItem color='success'>Room: {seat.room}</ListGroupItem>
             </ListGroup>
           ) : null}
         </div>
